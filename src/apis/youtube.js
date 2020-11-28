0cb1abdb1becb4dd145fc925628ef9a53dc1aae2
Tatiana Grigorieva <tsg1204@gmail.com>
2020-11-28 15:32:18 -0500

    add and remove key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = 'AIzaSyBfmX4eSHIm54cvG1ZjoAHzdRaVxluZMl8';
+const KEY = "YOUR_KEY";
  
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
